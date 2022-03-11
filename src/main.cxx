ac7227f7de45c1e6aae5b66dc74442808449eef2
Julia Reid <jureid@microsoft.com>
2022-03-11 10:33:26 -0500

    creating clang-format branch

--- src/main.cxx
+++ src/main.cxx
@@ -1,22 +1,25 @@
-#include <fmt/format.h>
 #include <cppitertools/itertools.hpp>
+#include <fmt/format.h>
 
-int fib(int n) {
-	int a = 0, b = 1;
+int fib(int n)
+{
+    int a = 0, b = 1;
+    int x;
 
-	for ([[maybe_unused]] int i : iter::range(n))
-	{
-		auto tmp = b;
-		b = a + b;
-		a = tmp;
-	}
+    for ([[maybe_unused]] int i : iter::range(n))
+    {
+        auto tmp = b;
+        b = a + b;
+        a = tmp;
+    }
 
-	return a;
+    return a;
 }
 
-int main() {
-	for (int i : iter::range(15))
-	{
-		fmt::print("fib({}) = {}\n", i, fib(i));
-	}
+int main()
+{
+    for (int i : iter::range(15))
+    {
+        fmt::print("fib({}) = {}\n", i, fib(i));
+    }
 }
